c364c083530a226dcfc0db3490a8080a5e78849f
namke-dev <namke.dev@gmail.com>
2023-12-18 22:52:24 +0700

    add comment

--- Data/DataExtensions.cs
+++ Data/DataExtensions.cs
@@ -6,6 +6,8 @@ public static class DataExtensions
 {
     public static void InitializeDb(this IServiceProvider serviceProvider)
     {
+        //In the provided code, the using statement is used for the CreateScope(), 
+        //which ensures that the scope is disposed of when the block is exited.
         using var scope = serviceProvider.CreateScope();
         var DbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         DbContext.Database.Migrate();
--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register service game access, defie service fife time as Singleton
+// Register service game access, 
+// defie service fife time as Singleton
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 
-//use builder.Configuration to read value in appsetting.json
+// Use builder.Configuration to read value in appsetting.json
 var connectionString = builder.Configuration.GetConnectionString("GameStoreContext");
 
-//Register EF service - The AddSqlServer method just like AddScope or AddSingleton, but we let EF handle it, but we can config those in DbContextOptions
+// Register EF service - The AddSqlServer method just like AddScope or AddSingleton, but we let EF handle it, but we can config those in DbContextOptions
 builder.Services.AddSqlServer<GameStoreContext>(connectionString);
 
 var app = builder.Build();
